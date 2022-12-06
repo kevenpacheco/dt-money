44f9c95f34440564d34e208715157cff9d144c99
Keven Pacheco <kevenpacheco2000@gmail.com>
2022-12-06 11:34:16 -0300

    refactor (Summary component): add horizontal scroll

--- src/components/Summary/styles.ts
+++ src/components/Summary/styles.ts
@@ -7,10 +7,22 @@ export const SummaryContainer = styled.section`
   padding: 0 1.5rem;
 
   display: grid;
-  grid-template-columns: repeat(auto-fit, minmax(260px, 1fr));
+  grid-template-columns: repeat(3, minmax(280px, 1fr));
   gap: 2rem;
 
   margin-top: -5rem;
+
+  overflow-x: scroll;
+  -ms-overflow-style: none; /* IE and Edge */
+  scrollbar-width: none; /* Firefox */
+
+  &::-webkit-scrollbar {
+    display: none;
+  }
+
+  @media (max-width: 768px) {
+    gap: 1rem;
+  }
 `
 
 interface SummaryCardProps {
@@ -35,9 +47,32 @@ export const SummaryCard = styled.div<SummaryCardProps>`
     font-size: 2rem;
   }
 
+  p {
+    display: none;
+    font-size: 0.875rem;
+    color: ${(props) => props.theme['gray-500']};
+    margin-top: 0.125rem;
+  }
+
   ${(props) =>
     props.variant === 'green' &&
     css`
       background: ${props.theme['green-700']};
+
+      p {
+        color: ${(props) => props.theme['gray-300']};
+      }
     `}
+
+  @media (max-width: 768px) {
+    p {
+      display: block;
+    }
+  }
+
+  @media (max-width: 375px) {
+    p {
+      display: block;
+    }
+  }
 `
