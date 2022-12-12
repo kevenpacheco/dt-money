a6702bf542202b26cd47458c68c5a87479622d94
Keven Pacheco <kevenpacheco2000@gmail.com>
2022-12-12 14:38:35 -0300

    refactor: create transaction card component

--- src/pages/Transactions/index.tsx
+++ src/pages/Transactions/index.tsx
@@ -2,15 +2,11 @@ import { useContextSelector } from 'use-context-selector'
 import { Header } from '../../components/Header'
 import { Summary } from '../../components/Summary'
 import { TransactionsContext } from '../../contexts/TransactionsContext'
-import { dateFormatter, priceFormatter } from '../../utils/formatter'
 import { DateFilterButton } from './components/DateFilterButton'
 import { Pagination } from './components/Pagination'
 import { SearchForm } from './components/SearchForm'
-import {
-  PriceHighlight,
-  TransactionsContainer,
-  TransactionsTable,
-} from './styles'
+import { TransactionCard } from './components/TransactionCard'
+import { TransactionsContainer, TransactionsTable } from './styles'
 
 export function Transactions() {
   const dataContext = useContextSelector(TransactionsContext, (context) => ({
@@ -54,19 +50,7 @@ export function Transactions() {
             <tbody>
               {transactions.map((transaction) => {
                 return (
-                  <tr key={transaction.id}>
-                    <td width="50%">{transaction.description}</td>
-                    <td>
-                      <PriceHighlight variant={transaction.type}>
-                        {transaction.type === 'outcome' && '- '}
-                        {priceFormatter.format(transaction.price)}
-                      </PriceHighlight>
-                    </td>
-                    <td>{transaction.category}</td>
-                    <td>
-                      {dateFormatter.format(new Date(transaction.createdAt))}
-                    </td>
-                  </tr>
+                  <TransactionCard key={transaction.id} data={transaction} />
                 )
               })}
             </tbody>
