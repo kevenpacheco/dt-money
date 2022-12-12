aaa93ee38a4191efed2d51c6810ab0f5d5df6b25
Keven Pacheco <kevenpacheco2000@gmail.com>
2022-12-12 18:31:09 -0300

    refactor: Adjust header height and negative margin the content

--- src/components/Header/styles.ts
+++ src/components/Header/styles.ts
@@ -2,7 +2,7 @@ import styled from 'styled-components'
 
 export const HeaderContainer = styled.header`
   background: ${(props) => props.theme['gray-900']};
-  padding: 2.5rem 0 7.5rem;
+  padding: 2.5rem 0 10.625rem;
 `
 
 export const HeaderContent = styled.div`
--- src/pages/Transactions/styles.ts
+++ src/pages/Transactions/styles.ts
@@ -3,7 +3,7 @@ import styled from 'styled-components'
 export const TransactionsContainer = styled.main`
   width: 100%;
   max-width: 1120px;
-  margin: -5rem auto 0;
+  margin: calc(2.5rem - 10.625rem) auto 0;
   padding: 0 1.5rem 2.5rem;
 `
 
